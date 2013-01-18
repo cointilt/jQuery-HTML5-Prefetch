045235bc22e5a455e36646d9556426199cf8de60
Will Ayers <willayers.net@gmail.com>
2013-01-18 12:31:51 -0700

    Updated with jslint

--- jquery.fetchIt.js
+++ jquery.fetchIt.js
@@ -5,31 +5,25 @@
  * @version 0.1
 */
 
-( function( $, document, undefined )
-	{
-		$.fn.fetchIt = function fetch_it_init( options ) {
-			// Default options
-			var defaults = {
-					'rel': 'prefetch prerender'
-				},
-				o = $.extend( defaults, options );
+(function ($, document, undefined) {
+	$.fn.fetchIt = function fetch_it_init(options) {
+		// Default options
+		var defaults = {
+				'rel': 'prefetch prerender'
+			},
+			o = $.extend(defaults, options);
 
-			return this.each(
+		return this.each(function fetch_it_loop() {
+			// Start Plugin here
 
-				function fetch_it_loop(){
-					// Start Plugin here
+			var $this = $(this),
+				prefetch_url = $this.attr('href');
 
-					var $this = $(this),
-						prefetch_url = $this.attr('href');
+			o[prefetch_url] = true;
 
-					o[prefetch_url] = true;
-
-					if ( prefetch_url && ! o[prefetch_url] )
-					{
-						$('<link />', { rel: options.rel, href: prefetch_url } ).appendTo( 'head' );
-					}
-				}
-			);
-		};
-	}
-) ( jQuery, document );
+			if (prefetch_url && ! o[prefetch_url]) {
+				$('<link />', {rel: options.rel, href: prefetch_url}).appendTo('head');
+			}
+		});
+	};
+}(jQuery, document));
